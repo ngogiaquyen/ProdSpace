9e455087c217d0a50a3ae5a6ddcbedbce2f4a0e9
ngogiaquyen <ngogiaquyendhtn223@gmail.com>
2025-06-28 20:59:04 +0700

    css module config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/dist/',
   base: './',
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, 'src'), // hoặc tùy bạn muốn alias đến đâu
+      '~': path.resolve(__dirname, 'src'),
     },
   },
-})
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    }
+  }
+});
