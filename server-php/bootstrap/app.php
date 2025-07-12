6b38822e2460d46c5b52d7d57e09bc474c7eae9f
ngogiaquyen <ngogiaquyendhtn223@gmail.com>
2025-07-12 16:51:22 +0700

    deploy

--- server-php/bootstrap/app.php
+++ server-php/bootstrap/app.php
@@ -9,6 +9,7 @@ return Application::configure(basePath: dirname(__DIR__))
          web: [
             __DIR__ . '/../routes/web.php',
             // __DIR__ . '/../routes/api-product.php', // Thêm file route ở đây
+            // __DIR__ . '/../routes/api-product.php', // Thêm file route ở đây
         ],
         // api: __DIR__ . '/../routes/api.php', // bỏ dòng này
         commands: __DIR__.'/../routes/console.php',
